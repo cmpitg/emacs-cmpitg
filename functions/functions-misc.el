118c34bdac8ab38a88798afa29a65a71c7e038aa
Duong H. Nguyen <cmpitg@gmail.com>
2014-02-06 17:58:00 +0700

    Add functions to interact with clipboard and kill ring

--- functions/functions-misc.el
+++ functions/functions-misc.el
@@ -25,4 +25,28 @@ Do nothing if server is already started."
   (unless (file-exists-p server-socket-dir)
     (server-start)))
 
+(defun ~clipboard<-region (begin end)
+  "Copy region to clipboard."
+  (clipboard-kill-ring-save begin end))
+
+(defun ~kill-ring<- (str)
+  "Copy a string to the kill ring."
+  (interactive "MString: ")
+  (kill-new str))
+
+(defun ~clipboard<- (str)
+  "Copy a string to clipboard."
+  (interactive "MString: ")
+  (let ((x-select-enable-clipboard t))
+    (x-select-text str)))
+
+(defun ~clipboard<-pwd ()
+  "Copy current directory to clipboard."
+  (interactive)
+  (~clipboard<- (~current-dir)))
+
 (defalias 'start-emacs-server '~start-emacs-server)
+(defalias 'clipboard<-region '~clipboard<-region)
+(defalias 'kill-ring<- '~kill-ring<-)
+(defalias 'clipboard<- '~clipboard<-)
+(defalias 'clipboard<-pwd '~clipboard<-pwd)
