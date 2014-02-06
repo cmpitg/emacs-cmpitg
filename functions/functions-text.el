7654c33c2187b77eff618c07ce3c01d71e1af501
Duong H. Nguyen <cmpitg@gmail.com>
2014-02-06 11:17:31 +0700

    Prefix: - -> ~

--- functions/functions-text.el
+++ functions/functions-text.el
@@ -15,7 +15,7 @@
 ;; with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 
-(defun surround (begin-string end-string)
+(defun ~surround (begin-string end-string)
   "Surround current selection with `begin-string` at the
 beginning and `end-string` at the end.  If selection is not
 active, insert `begin-string` and `end-string` and place the
@@ -51,7 +51,7 @@ cursor in-between them."
   (interactive)
   (surround "`" "`"))
 
-(defun -move-to-beginning-of-line ()
+(defun ~move-to-beginning-of-line ()
   "Move point back to indentation of beginning of line.
 
 Move point to the first non-whitespace character on this line.
@@ -63,7 +63,7 @@ If point is already there, move to the beginning of the line."
     (when (= orig-point (point))
       (move-beginning-of-line nil))))
 
-(defun -duplicate-line ()
+(defun ~duplicate-line ()
   "Duplicate current line."
   (interactive)
   (beginning-of-line)
@@ -74,7 +74,7 @@ If point is already there, move to the beginning of the line."
   (beginning-of-line)
   (previous-line))
 
-(defun -open-line (arg)
+(defun ~open-line (arg)
   "Open line and move to the next line."
   (interactive "p")
   (end-of-line)
@@ -83,14 +83,14 @@ If point is already there, move to the beginning of the line."
   (next-line 1)
   (indent-according-to-mode))
 
-(defun -open-line-before (arg)
+(defun ~open-line-before (arg)
   "Open line and move to the previous line."
   (interactive "p")
   (beginning-of-line)
   (open-line arg)
   (indent-according-to-mode))
 
-(defun -toggle-letter-case ()
+(defun ~toggle-letter-case ()
   "Toggle the letter case of current word or text selection.
 Toggles from 3 cases: UPPER CASE, lower case, Title Case, in that
 cyclic order."
@@ -125,12 +125,12 @@ cyclic order."
       (downcase-region pos1 pos2)
       (put this-command 'state "all lower")))))
 
-(defun -fix-hard-wrapped-region (begin end)
+(defun ~fix-hard-wrapped-region (begin end)
   "Fix hard-wrapped paragraphs."
   (interactive "r")
   (shell-command-on-region begin end "fmt -w 2500" nil t))
 
-(defun -mark-word ()
+(defun ~mark-word ()
   "Put point at beginning of current word, set mark at end."
   (interactive)
   (let* ((opoint (point))
@@ -147,40 +147,40 @@ cyclic order."
              (goto-char (match-beginning 0)))
       (error "No word at point" word))))
 
-(defun -mark-line ()
+(defun ~mark-line ()
   "Mark current line."
   (interactive)
   (beginning-of-line)
   (push-mark (point) t t)
   (end-of-line))
 
-(defun -insert-me ()
+(defun ~insert-me ()
   "Insert my information."
   (interactive)
   (insert *me*))
 
-(defun -delete-line ()
+(defun ~delete-line ()
   "Delete current line."
   (interactive)
   (beginning-of-line)
   (kill-line)
   (kill-line))
 
-(defun -get-text (start end)
+(defun ~get-text (start end)
   "Return text from current buffer between start and end point."
   (if (or (< start (point-min))
           (< (point-max) end))
     ""
     (buffer-substring start end)))
 
-(defun -current-char ()
+(defun ~current-char ()
   "Return the string representing the character at the current
 cursor position."
-  (-get-text (point) (+ 1 (point))))
+  (~get-text (point) (+ 1 (point))))
 
-(defun -peek-char ()
+(defun ~peek-char ()
   "Peek next character, return the string representing it.."
-  (-get-text (+ 1 (point)) (+ 2 (point))))
+  (~get-text (+ 1 (point)) (+ 2 (point))))
 
 (defun join-with-next-line ()
   "Join next line with the current line.  This is just a
@@ -200,7 +200,7 @@ convenient wrapper of `join-line'."
   "Determine if a selection is being held."
   (region-active-p))
 
-(defun -current-selection ()
+(defun ~current-selection ()
   "Return the current selected text."
   (if (is-selecting?)
     (buffer-substring (selection-start)
@@ -209,9 +209,9 @@ convenient wrapper of `join-line'."
 
 (defun get-selection ()
   "Return the current selected text."
-  (-current-selection))
+  (~current-selection))
 
-(defun -delete-selected-text ()
+(defun ~delete-selected-text ()
   "Delete the selected text, do nothing if none text is selected."
   (if (is-selecting?)
     (delete-region (selection-start) (selection-end))))
@@ -247,3 +247,5 @@ text.  Move the cursor to the new line."
         (save-excursion (newline-and-indent)))
     (newline arg)
     (indent-according-to-mode)))
+
+(defalias 'surround '~surround)
