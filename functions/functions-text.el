8994de1e19ebfd1f649fcc8224faf72ae9447ecb
Duong H. Nguyen <cmpitg@gmail.com>
2014-02-06 10:45:55 +0700

    Adjust behavior of RET, use electric RET

--- functions/functions-text.el
+++ functions/functions-text.el
@@ -236,3 +236,14 @@ do nothing."
 do nothing."
   (if (is-selecting?)
     (goto-point (selection-end))))
+
+(defun electrify-return-if-match (arg)
+  "If the text after the cursor matches `electrify-return-match'
+then open and indent an empty line between the cursor and the
+text.  Move the cursor to the new line."
+  (interactive "P")
+  (let ((case-fold-search nil))
+    (if (looking-at *electrify-return-match*)
+        (save-excursion (newline-and-indent)))
+    (newline arg)
+    (indent-according-to-mode)))
