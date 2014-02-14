f2888a4f3ee06e1ef33d38dbf557cc758bfe5e9e
Duong H. Nguyen <cmpitg@gmail.com>
2014-02-14 13:17:42 +0700

    Add function to handle network

--- load-functions.el
+++ load-functions.el
@@ -1275,6 +1275,77 @@ Example:
 ;; with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 
+(defun ~parse-tramp-argument (connection-string)
+  "Return an alist with
+
+* protocol
+* username
+* host
+* port
+* path
+
+from a Tramp connection string.
+
+E.g.
+
+\(~parse-tramp-argument \"/ssh:cmpitg@192.168.1.4#3355:/etc/network/interfaces\"\)
+;; =>
+;; \(\(protocol . \"ssh\"\)
+;;  \(username . \"cmpitg\"\)
+;;  \(host . \"192.168.1.4\"\)
+;;  \(port . \"3355\"\)
+;;  \(path . \"/etc/network/interfaces\"\)\)
+"
+  (flet ((get-path (host-and-path)
+                   (if (string-match (rx (group "/" (1+ anything))) host-and-path)
+                       (match-string 1 host-and-path)
+                     "/tmp/"))
+         (get-port (host-and-path)
+                   (if (string-match (rx (1+ (not (any "\\")))
+                                         "#"
+                                         (group (1+ digit)))
+                                     host-and-path)
+                       (match-string 1 host-and-path)
+                     "22"))
+         (get-host (host-and-path)
+                   (if (string-match (rx bol
+                                         (group (1+ (not (any "#" ":")))))
+                                     host-and-path)
+                       (match-string 1 host-and-path)
+                     "localhost")))
+
+    (string-match "^/\\([^:]+\\):\\([^@]+\\)@\\(.*\\)$" connection-string)
+
+    (let* ((protocol      (match-string 1 connection-string))
+           (username      (match-string 2 connection-string))
+           (host-and-path (match-string 3 connection-string))
+           
+           (host          (get-host host-and-path))
+           (port          (get-port host-and-path))
+           (path          (get-path host-and-path)))
+      `((protocol . ,protocol)
+        (username . ,username)
+        (host     . ,host)
+        (port     . ,port)
+        (path     . ,path)))))
+
+;;
+;; Copyright (C) 2014 Duong Nguyen ([@cmpitg](https://github.com/cmpitg/))
+;;
+;; This project is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; This project is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+
 (defun ~el-get-package-list ()
   "Get the list of packages cached in el-get repositories.  This
 function doesn't update el-get database.  Returns a plist of format
