24bda9c3f0438d632b120254831531bf522ae908
Duong H. Nguyen <cmpitg@gmail.com>
2014-02-17 16:46:39 +0700

    Add default dot-hyde

---
+++ samples/dot-hyde.el
@@ -0,0 +1,5 @@
+(setq hyde/git/remote "origin"          ; The name of the remote to which we should push
+      hyde/git/remote "master"          ; The name of the branch on which your blog
+                                        ; resides
+      hyde/deploy-command  "git push"   ; Command to deploy
+      )
