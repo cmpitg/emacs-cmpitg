b46415d62168d8c6824974b9d0b72d5e19bb6854
Duong H. Nguyen <cmpitg@gmail.com>
2014-03-12 22:46:07 +0700

    Add powerful flexible matching for hippie-expand

---
+++ recipes/environment/50-hippie-expand.el
@@ -0,0 +1,79 @@
+;;
+;; Copyright (C) 2012-2014 Duong Nguyen ([@cmpitg](https://github.com/cmpitg/))
+;;
+;; This project is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; This project is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+
+(defadvice he-substitute-string (after he-paredit-fix)
+  "Remove extra paren when expanding line in Paredit"
+  (when (and paredit-mode
+             (equal (substring str -1) ")"))
+    (backward-delete-char 1)
+    (forward-char)))
+
+(defun try-expand-flexible-abbrev (old)
+  "Try to complete word using flexible matching.
+
+Flexible matching works by taking the search string and then
+interspersing it with a regexp for any character. So, if you try
+to do a flexible match for `foo' it will match the word
+`findOtherOtter' but also `fixTheBoringOrange' and
+`ifthisisboringstopreadingnow'.
+
+The argument OLD has to be nil the first call of this function, and t
+for subsequent calls (for further possible completions of the same
+string).  It returns t if a new completion is found, nil otherwise."
+  (unless old
+    (he-init-string (he-lisp-symbol-beg) (point))
+    (if (not (he-string-member he-search-string he-tried-table))
+        (setq he-tried-table (cons he-search-string he-tried-table)))
+    (setq he-expand-list
+          (and (not (equal he-search-string ""))
+               (he-flexible-abbrev-collect he-search-string))))
+
+  (while (and he-expand-list
+              (he-string-member (car he-expand-list) he-tried-table))
+    (setq he-expand-list (cdr he-expand-list)))
+
+  (cond ((null he-expand-list)
+         (if old
+             (he-reset-string))
+         nil)
+        (t
+         (he-substitute-string (car he-expand-list))
+         (setq he-expand-list (cdr he-expand-list))
+         t)))
+
+(defun he-flexible-abbrev-collect (str)
+  "Find and collect all words that flex-matches STR.
+See docstring for `try-expand-flexible-abbrev' for information
+about what flexible matching means in this context."
+  (let ((collection nil)
+        (regexp (he-flexible-abbrev-create-regexp str)))
+    (save-excursion
+      (goto-char (point-min))
+      (while (search-forward-regexp regexp nil t)
+        ;; Is there a better or quicker way than using
+        ;; `thing-at-point' here?
+        (setq collection (cons (thing-at-point 'symbol) collection))))
+    collection))
+
+(defun he-flexible-abbrev-create-regexp (str)
+  "Generate regexp for flexible matching of STR.
+See docstring for `try-expand-flexible-abbrev' for information
+about what flexible matching means in this context."
+  (concat "\\b" (mapconcat (lambda (x) (concat "\\w*" (list x))) str "")
+          "\\w*-*" "\\b"))
+
+(add-to-list 'hippie-expand-try-functions-list 'try-expand-flexible-abbrev)
