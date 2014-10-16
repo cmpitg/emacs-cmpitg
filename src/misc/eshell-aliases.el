5a06975da3eaec61e78b18775b7090d5694b2a37
Duong H. Nguyen <cmpitg@gmail.com>
2014-10-16 11:59:47 +0700

    Some of my convenient shell aliases

--- src/misc/eshell-aliases.el
+++ src/misc/eshell-aliases.el
@@ -1,4 +1,5 @@
-alias lns ln -s *
-alias tosrc cd /m/src/
-alias ll ls -lahF $*
+alias pks apt-cache search $*
 alias ff find-file $*
+alias ll ls -lahF $*
+alias tosrc cd /m/src/
+alias lns ln -s *
