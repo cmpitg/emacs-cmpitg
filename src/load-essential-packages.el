0bd042b3144f912b0abea24b41b05d002668c4f2
Duong H. Nguyen <cmpitg@gmail.com>
2015-04-15 19:20:18 +0700

    Warning: Disable tabbar ruler as it makes X.org consumes CPU extensively

--- src/load-essential-packages.el
+++ src/load-essential-packages.el
@@ -469,17 +469,17 @@ current word at point as initial string."
 ;; Simple tabbar
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package tabbar-ruler
-  :ensure tabbar-ruler
-  :config (progn
-            (setq tabbar-ruler-global-tabbar t) ; If you want tabbar
-            ;; (setq tabbar-ruler-global-ruler t)     ; if you want a global ruler
-            ;; (setq tabbar-ruler-popup-menu t)       ; If you want a popup menu.
-            ;; (setq tabbar-ruler-popup-toolbar t)    ; If you want a popup toolbar
-            ;; (setq tabbar-ruler-popup-scrollbar t)) ; If you want to only show
-            ;;                                        ; the scroll bar when your
-            ;;                                        ; mouse is moving.
-            ))
+; (use-package tabbar-ruler
+;   :ensure tabbar-ruler
+;   :config (progn
+;             (setq tabbar-ruler-global-tabbar t) ; If you want tabbar
+;             ;; (setq tabbar-ruler-global-ruler t)     ; if you want a global ruler
+;             ;; (setq tabbar-ruler-popup-menu t)       ; If you want a popup menu.
+;             ;; (setq tabbar-ruler-popup-toolbar t)    ; If you want a popup toolbar
+;             ;; (setq tabbar-ruler-popup-scrollbar t)) ; If you want to only show
+;             ;;                                        ; the scroll bar when your
+;             ;;                                        ; mouse is moving.
+;             ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Better ack interface
