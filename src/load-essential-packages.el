64aaadf4f141cb7a7e64208146b618d56551dfff
Ha-Duong Nguyen <haduong.nguyen@dreambroker.com>
2016-08-25 15:57:27 +0300

    feat: ~swoop-bare is no longer needed

--- src/load-essential-packages.el
+++ src/load-essential-packages.el
@@ -123,21 +123,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package thingatpt
-  :ensure thingatpt)
+  :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Multiple cursors
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package multiple-cursors
-  :ensure multiple-cursors)
+  :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Expand region
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package expand-region
-  :ensure expand-region
+  :ensure t
   :commands er/expand-region)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -145,7 +145,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package eldoc
-  :ensure eldoc
+  :ensure t
   :diminish eldoc-mode
   :config (progn
             (eldoc-add-command 'paredit-backward-delete
@@ -172,11 +172,11 @@
 ;; https://github.com/m2ym/popwin-el
 
 (use-package popwin
-  :ensure popwin
+  :ensure t
   :config (progn
             (popwin-mode 1)
             (setq anything-samewindow nil)
-            
+
             (dolist (el '(("\*anything*" :regexp t :height 25)
                           ("*anything*" :height 20)
                           (dired-mode :position top)
@@ -201,17 +201,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package dired+
-  :ensure dired+
+  :ensure t
   :init (progn
           (setq dired-listing-switches "-lahF")
           ;; Reuse current buffer when opening file/dir
           (toggle-diredp-find-file-reuse-dir 1)))
 
 (use-package dired-details+
-  :ensure dired-details+
+  :ensure t
   :init (progn
           (use-package dired-single
-            :ensure dired-single))
+            :ensure t))
   :config (progn
             (setq dired-listing-switches "-lhFgG --group-directories-first")))
 
@@ -220,14 +220,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package tar-mode
-  :ensure tar-mode)
+  :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Save and restore current editing point
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package saveplace
-  :ensure saveplace
+  :ensure t
   :init (progn
           (setq-default save-place t)))
 
@@ -236,46 +236,46 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package color-theme
-  :ensure color-theme)
+  :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Recent files
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package recentf
-  :ensure recentf
-  :commands recentf-open-files
-  ; :idle (recentf-mode)
-  ; :idle-priority 3
+  :ensure t
   :init (progn
           (recentf-mode 1)
-          (setq recentf-max-menu-items 128)))
+          (setq recentf-max-menu-items 256)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Smoother scrolling
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package smooth-scrolling
-  :ensure smooth-scrolling)
+  :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Better ido
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Helm mode seems to be much convenient than ido
+;;
 
-(use-package ido
-  :ensure ido
-  :config (progn
-            (ido-mode 1)
-            (ido-everywhere 1)
-            (use-package flx-ido
-              :config (progn
-                        (flx-ido-mode 1)
+;; (use-package ido
+;;   :ensure ido
+;;   :config (progn
+;;             (ido-mode 1)
+;;             (ido-everywhere 1)
+;;             (use-package flx-ido
+;;               :config (progn
+;;                         (flx-ido-mode 1)
 
-                        ;; disable ido faces to see flx highlights.
-                        (setq ido-use-faces nil)))
-            (use-package ido-vertical-mode
-              :ensure ido-vertical-mode
-              :config (ido-vertical-mode 1))))
+;;                         ;; disable ido faces to see flx highlights.
+;;                         (setq ido-use-faces nil)))
+;;             (use-package ido-vertical-mode
+;;               :ensure ido-vertical-mode
+;;               :config (ido-vertical-mode 1))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Custom unique naming method
@@ -292,52 +292,63 @@
 ;; `smartscan-symbol-go-backward'  M-p
 
 (use-package smartscan
-  :ensure smartscan
+  :ensure t
   :config (progn
             (global-smartscan-mode 1)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Open with external program
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;; Load from local
 ;;
+;; Seems to be abandoned.  We'll find another way to provide the openwith
+;; functionality.
+;;
 
-(use-package openwith
-  :init (progn
-          (openwith-mode t)))
+;; (use-package openwith
+;;   :config (progn
+;;             (openwith-mode t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Better M-x
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; helm-M-x should be better than smex
+;;
 
-(use-package smex
-  :ensure smex
-  :init (progn
-          (smex-initialize)))
+;; (use-package smex
+;;   :ensure t
+;;   :init (progn
+;;           (smex-initialize)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Asciidoc
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package adoc-mode
-  :ensure adoc-mode
+  :ensure t
   :defer t
   :commands adoc-mode
   :init (progn
-          (~auto-load-mode '("\\.ascii" "\\.txt" "\\.adoc") 'adoc-mode)
+          (~auto-load-mode '("\\.ascii" "\\.adoc") 'adoc-mode)
           (add-hook 'adoc-mode-hook 'auto-fill-mode)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Find file with fuzzy matching
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Helm is mature and advanced and serves the needs well.  Perhaps I'll come
+;; back to fiplr some time.
+;;
 
-(use-package fiplr
-  :ensure fiplr
-  :config (progn
-            (add-to-list 'fiplr-root-markers "README.md")
-            (add-to-list 'fiplr-root-markers "README.adoc")
-            (add-to-list 'fiplr-root-markers "README.txt")
-            (add-to-list 'fiplr-root-markers "README")))
+;; (use-package fiplr
+;;   :ensure fiplr
+;;   :config (progn
+;;             (add-to-list 'fiplr-root-markers "README.md")
+;;             (add-to-list 'fiplr-root-markers "README.adoc")
+;;             (add-to-list 'fiplr-root-markers "README.txt")
+;;             (add-to-list 'fiplr-root-markers "README")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Editable Ack
@@ -371,114 +382,117 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Buffer navigation with pattern matching and replacing
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Seems unstable, needs more evaluation
+;;
 
 (use-package swoop
-  :ensure swoop
+  :ensure t
   :commands swoop
   :init (use-package helm-swoop
           :ensure helm-swoop))
 
-(defun ~swoop-bare (&optional $query)
-  "Search through words within the current buffer.  Don't take
-current word at point as initial string."
-  (interactive)
-  (if current-prefix-arg
-      (swoop-core :$resume t :$query swoop-last-query-plain)
-    (swoop-core :$query "")))
+;; (defun ~swoop-bare (&optional $query)
+;;   "Search through words within the current buffer.  Don't take
+;; current word at point as initial string."
+;;   (interactive)
+;;   (if current-prefix-arg
+;;       (swoop-core :$resume t :$query swoop-last-query-plain)
+;;     (swoop-core :$query "")))
 
-(defun* ~helm-swoop-bare (&key $query $source ($multiline current-prefix-arg))
-  "Search through words within the current buffer.  Don't take
-current word at point as initial string."
-  (interactive)
-  (setq helm-swoop-synchronizing-window (selected-window))
-  (setq helm-swoop-last-point (cons (point) (buffer-name (current-buffer))))
-  (setq helm-swoop-last-line-info
-        (cons (current-buffer) (line-number-at-pos)))
-  (unless (boundp 'helm-swoop-last-query)
-    (set (make-local-variable 'helm-swoop-last-query) ""))
-  (setq helm-swoop-target-buffer (current-buffer))
-  (helm-swoop--set-prefix (prefix-numeric-value $multiline))
-  ;; Overlay
-  (setq helm-swoop-line-overlay (make-overlay (point) (point)))
-  (overlay-put helm-swoop-line-overlay
-               'face (if (< 1 helm-swoop-last-prefix-number)
-                         'helm-swoop-target-line-block-face
-                       'helm-swoop-target-line-face))
-  ;; Cache
-  (cond ((not (boundp 'helm-swoop-cache))
-         (set (make-local-variable 'helm-swoop-cache) nil))
-        ((buffer-modified-p)
-         (setq helm-swoop-cache nil)))
-  ;; Cache for multiline
-  (cond ((not (boundp 'helm-swoop-list-cache))
-         (set (make-local-variable 'helm-swoop-list-cache) nil))
-        ((buffer-modified-p)
-         (setq helm-swoop-list-cache nil)))
-  (unwind-protect
-      (progn
-        ;; For synchronizing line position
-        (ad-enable-advice 'helm-next-line 'around 'helm-swoop-next-line)
-        (ad-activate 'helm-next-line)
-        (ad-enable-advice 'helm-previous-line 'around 'helm-swoop-previous-line)
-        (ad-activate 'helm-previous-line)
-        (ad-enable-advice 'helm-move--next-line-fn 'around
-                          'helm-multi-swoop-next-line-cycle)
-        (ad-activate 'helm-move--next-line-fn)
-        (ad-enable-advice 'helm-move--previous-line-fn 'around
-                          'helm-multi-swoop-previous-line-cycle)
-        (ad-activate 'helm-move--previous-line-fn)
-        (add-hook 'helm-update-hook 'helm-swoop--pattern-match)
-        (add-hook 'helm-after-update-hook 'helm-swoop--keep-nearest-position t)
-        (unless (and (symbolp 'helm-match-plugin-mode)
-                     (symbol-value 'helm-match-plugin-mode))
-          (helm-match-plugin-mode 1))
-        (cond ($query
-               (if (string-match
-                    "\\(\\^\\[0\\-9\\]\\+\\.\\)\\(.*\\)" $query)
-                   $query ;; NEED FIX #1 to appear as a "^"
-                 $query))
-              (mark-active
-               (let (($st (buffer-substring-no-properties
-                           (region-beginning) (region-end))))
-                 (if (string-match "\n" $st)
-                     (message "Multi line region is not allowed")
-                   (setq $query (helm-swoop-pre-input-optimize $st)))))
-              (t (setq $query "")))
-        ;; First behavior
-        (helm-swoop--recenter)
-        (move-beginning-of-line 1)
-        (helm-swoop--target-line-overlay-move)
-        ;; Execute helm
-        (let ((helm-display-function helm-swoop-split-window-function)
-              (helm-display-source-at-screen-top nil)
-              (helm-completion-window-scroll-margin 5))
-          (helm :sources
-                (or $source
-                    (if (> helm-swoop-last-prefix-number 1)
-                        (helm-c-source-swoop-multiline helm-swoop-last-prefix-number)
-                      (helm-c-source-swoop)))
-                :buffer helm-swoop-buffer
-                :input $query
-                :prompt helm-swoop-prompt
-                :preselect
-                ;; Get current line has content or else near one
-                (if (string-match "^[\t\n\s]*$" (helm-swoop--get-string-at-line))
-                    (save-excursion
-                      (if (re-search-forward "[^\t\n\s]" nil t)
-                          (format "^%s\s" (line-number-at-pos))
-                        (re-search-backward "[^\t\n\s]" nil t)
-                        (format "^%s\s" (line-number-at-pos))))
-                  (format "^%s\s" (line-number-at-pos)))
-                :candidate-number-limit helm-swoop-candidate-number-limit)))
-    ;; Restore helm's hook and window function etc
-    (helm-swoop--restore)))
+;; (defun* ~helm-swoop-bare (&key $query $source ($multiline current-prefix-arg))
+;;   "Search through words within the current buffer.  Don't take
+;; current word at point as initial string."
+;;   (interactive)
+;;   (setq helm-swoop-synchronizing-window (selected-window))
+;;   (setq helm-swoop-last-point (cons (point) (buffer-name (current-buffer))))
+;;   (setq helm-swoop-last-line-info
+;;         (cons (current-buffer) (line-number-at-pos)))
+;;   (unless (boundp 'helm-swoop-last-query)
+;;     (set (make-local-variable 'helm-swoop-last-query) ""))
+;;   (setq helm-swoop-target-buffer (current-buffer))
+;;   (helm-swoop--set-prefix (prefix-numeric-value $multiline))
+;;   ;; Overlay
+;;   (setq helm-swoop-line-overlay (make-overlay (point) (point)))
+;;   (overlay-put helm-swoop-line-overlay
+;;                'face (if (< 1 helm-swoop-last-prefix-number)
+;;                          'helm-swoop-target-line-block-face
+;;                        'helm-swoop-target-line-face))
+;;   ;; Cache
+;;   (cond ((not (boundp 'helm-swoop-cache))
+;;          (set (make-local-variable 'helm-swoop-cache) nil))
+;;         ((buffer-modified-p)
+;;          (setq helm-swoop-cache nil)))
+;;   ;; Cache for multiline
+;;   (cond ((not (boundp 'helm-swoop-list-cache))
+;;          (set (make-local-variable 'helm-swoop-list-cache) nil))
+;;         ((buffer-modified-p)
+;;          (setq helm-swoop-list-cache nil)))
+;;   (unwind-protect
+;;       (progn
+;;         ;; For synchronizing line position
+;;         (ad-enable-advice 'helm-next-line 'around 'helm-swoop-next-line)
+;;         (ad-activate 'helm-next-line)
+;;         (ad-enable-advice 'helm-previous-line 'around 'helm-swoop-previous-line)
+;;         (ad-activate 'helm-previous-line)
+;;         (ad-enable-advice 'helm-move--next-line-fn 'around
+;;                           'helm-multi-swoop-next-line-cycle)
+;;         (ad-activate 'helm-move--next-line-fn)
+;;         (ad-enable-advice 'helm-move--previous-line-fn 'around
+;;                           'helm-multi-swoop-previous-line-cycle)
+;;         (ad-activate 'helm-move--previous-line-fn)
+;;         (add-hook 'helm-update-hook 'helm-swoop--pattern-match)
+;;         (add-hook 'helm-after-update-hook 'helm-swoop--keep-nearest-position t)
+;;         (unless (and (symbolp 'helm-match-plugin-mode)
+;;                      (symbol-value 'helm-match-plugin-mode))
+;;           (helm-match-plugin-mode 1))
+;;         (cond ($query
+;;                (if (string-match
+;;                     "\\(\\^\\[0\\-9\\]\\+\\.\\)\\(.*\\)" $query)
+;;                    $query ;; NEED FIX #1 to appear as a "^"
+;;                  $query))
+;;               (mark-active
+;;                (let (($st (buffer-substring-no-properties
+;;                            (region-beginning) (region-end))))
+;;                  (if (string-match "\n" $st)
+;;                      (message "Multi line region is not allowed")
+;;                    (setq $query (helm-swoop-pre-input-optimize $st)))))
+;;               (t (setq $query "")))
+;;         ;; First behavior
+;;         (helm-swoop--recenter)
+;;         (move-beginning-of-line 1)
+;;         (helm-swoop--target-line-overlay-move)
+;;         ;; Execute helm
+;;         (let ((helm-display-function helm-swoop-split-window-function)
+;;               (helm-display-source-at-screen-top nil)
+;;               (helm-completion-window-scroll-margin 5))
+;;           (helm :sources
+;;                 (or $source
+;;                     (if (> helm-swoop-last-prefix-number 1)
+;;                         (helm-c-source-swoop-multiline helm-swoop-last-prefix-number)
+;;                       (helm-c-source-swoop)))
+;;                 :buffer helm-swoop-buffer
+;;                 :input $query
+;;                 :prompt helm-swoop-prompt
+;;                 :preselect
+;;                 ;; Get current line has content or else near one
+;;                 (if (string-match "^[\t\n\s]*$" (helm-swoop--get-string-at-line))
+;;                     (save-excursion
+;;                       (if (re-search-forward "[^\t\n\s]" nil t)
+;;                           (format "^%s\s" (line-number-at-pos))
+;;                         (re-search-backward "[^\t\n\s]" nil t)
+;;                         (format "^%s\s" (line-number-at-pos))))
+;;                   (format "^%s\s" (line-number-at-pos)))
+;;                 :candidate-number-limit helm-swoop-candidate-number-limit)))
+;;     ;; Restore helm's hook and window function etc
+;;     (helm-swoop--restore)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; URL shortener
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package url-shortener
-  :ensure url-shortener
+  :ensure t
   :defer t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
