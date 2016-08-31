1a99d2dcb33ce39e4fb7ca271c807293305e7f6f
Ha-Duong Nguyen <cmpitg@gmail.com>
2016-08-31 12:20:02 +0300

    Add a function to toggle dedicated window manually

---
+++ src/personal-toggle-window-dedicated.el
@@ -0,0 +1,40 @@
+;;
+;; Copyright (C) 2014-2016 Duong Nguyen (@cmpitg)
+;;
+;; This project is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; This project is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+;; more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+
+;; Shamelessly steal from http://stackoverflow.com/a/5182111/219881
+
+(defadvice pop-to-buffer (before cancel-other-window first)
+  (ad-set-arg 1 nil))
+
+(ad-activate 'pop-to-buffer)
+
+;; Toggle window dedication
+(defun toggle-window-dedicated ()
+  "Toggle whether the current active window is dedicated or not"
+  (interactive)
+  (message
+   (if (let (window (get-buffer-window (current-buffer)))
+         (set-window-dedicated-p window 
+                                 (not (window-dedicated-p window))))
+       "Window '%s' is dedicated"
+     "Window '%s' is normal")
+   (current-buffer)))
+
+;; Press [pause] key in each window you want to "freeze"
+(global-set-key [pause] 'toggle-window-dedicated)
+
+(provide 'ee:personal-toggle-dedicated-window)
