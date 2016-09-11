1a53667e800d0910b6f50143412b710d9a34b584
Ha-Duong Nguyen <cmpitg@gmail.com>
2016-09-11 15:52:52 +0300

    refactor: Remove redundant spaces @ ~add-emacs-lisp-header

--- src/functions.el
+++ src/functions.el
@@ -983,56 +983,56 @@ E.g.
 (defun ~add-emacs-lisp-header ()
   "Add Emacs Lisp header."
   (interactive)
-  (insert ";;; mylib.el --- 
-;; 
+  (insert ";;; mylib.el ---
+;;
 ;; Filename: mylib.el
-;; Description: 
-;; Author: 
-;; Maintainer: 
+;; Description:
+;; Author:
+;; Maintainer:
 ;; Copyright (C) 2014  Duong Nguyen
 ;; Created: $ date -R
-;; Version: 
-;; Last-Updated: 
-;;           By: 
+;; Version:
+;; Last-Updated:
+;;           By:
 ;;     Update #: 0
-;; URL: 
-;; Keywords: 
-;; Compatibility: 
-;; 
+;; URL:
+;; Keywords:
+;; Compatibility:
+;;
 ;; Features that might be required by this library:
 ;;
 ;;   None
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
-;;; Commentary: 
-;; 
-;; 
-;; 
+;;
+;;; Commentary:
+;;
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Change log:
-;; 
-;; 
+;;
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
 ;; published by the Free Software Foundation; either version 3, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
-;; 
+;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 
+;;
 ;;; Code:
 
 
