fb7633d599504124a21d0cb2472fcc2617df1c60
Ha-Duong Nguyen <cmpitg@gmail.com>
2016-11-12 23:58:50 +0200

    Enhance Python interpreter

--- src/cmpitg-flavored-packages.el
+++ src/cmpitg-flavored-packages.el
@@ -321,7 +321,18 @@
                         (venv-initialize-interactive-shells)
                         (venv-initialize-eshell)
                         (setq venv-location (or (getenv "WORKON_HOME")
-                                                "/m/virtual-envs/"))))))
+                                                "/m/virtual-envs/"))))
+
+            (setq python-shell-interpreter "ipython"
+                  python-shell-interpreter-args ""
+                  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+                  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+                  python-shell-completion-setup-code
+                  "from IPython.core.completerlib import module_completion"
+                  python-shell-completion-module-string-code
+                  "';'.join(module_completion('''%s'''))\n"
+                  python-shell-completion-string-code
+                  "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")))
 
 ;;
 ;; Company mode support in Jedi is weak
