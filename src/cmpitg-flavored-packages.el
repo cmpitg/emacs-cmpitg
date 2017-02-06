5d0cefdf2343a154c117bdc6a6d7583a55611ff9
Ha-Duong Nguyen <cmpitg@gmail.com>
2017-02-06 22:32:59 +0200

    feat: Temporarily tryout default Python interpreter

--- src/cmpitg-flavored-packages.el
+++ src/cmpitg-flavored-packages.el
@@ -327,16 +327,17 @@
                         (setq venv-location (or (getenv "WORKON_HOME")
                                                 "/m/virtual-envs/"))))
 
-            (setq python-shell-interpreter "ipython"
-                  python-shell-interpreter-args ""
-                  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-                  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-                  python-shell-completion-setup-code
-                  "from IPython.core.completerlib import module_completion"
-                  python-shell-completion-module-string-code
-                  "';'.join(module_completion('''%s'''))\n"
-                  python-shell-completion-string-code
-                  "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")))
+            ;; (setq python-shell-interpreter "ipython"
+            ;;       python-shell-interpreter-args ""
+            ;;       python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+            ;;       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+            ;;       python-shell-completion-setup-code
+            ;;       "from IPython.core.completerlib import module_completion"
+            ;;       python-shell-completion-module-string-code
+            ;;       "';'.join(module_completion('''%s'''))\n"
+            ;;       python-shell-completion-string-code
+            ;;       "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+            ))
 
 ;;
 ;; Company mode support in Jedi is weak
