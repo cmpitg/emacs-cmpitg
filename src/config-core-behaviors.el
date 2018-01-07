135abdc9b800a5344fd3e770c7efee9d898be940
Ha-Duong Nguyen <cmpitg@gmail.com>
2018-01-07 12:36:47 +0200

    feat: Add saved-last-session to init-bare

--- src/config-core-behaviors.el
+++ src/config-core-behaviors.el
@@ -55,6 +55,20 @@
 (use-package saveplace
   :config (save-place-mode 1))
 
+;; Open last session/visited files from the last session
+;; Ref: https://github.com/nflath/save-visited-files
+(use-package save-visited-files
+  :config (progn
+            ;; Each Emacs server has a different list of visited files
+            (setq save-visited-files-location
+                  (format "~/.emacs.d/emacs-visited-files.%s"
+                          (~emacs-server-name)))
+
+            (unless (file-exists-p save-visited-files-location)
+              (write-region "" nil save-visited-files-location))
+
+            (turn-on-save-visited-files-mode)))
+
 ;; Disable Tramp autosave
 (setq tramp-auto-save-directory "/tmp/")
 
