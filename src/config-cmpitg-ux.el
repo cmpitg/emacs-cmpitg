dea660745e9bff877410e5466f861012b34fc0d2
Ha-Duong Nguyen <cmpitg@gmail.com>
2018-01-13 13:50:38 +0200

    feat: Add lexical binding to config-cmpitg-ux

--- src/config-cmpitg-ux.el
+++ src/config-cmpitg-ux.el
@@ -1,5 +1,6 @@
-;;
-;; Copyright (C) 2014-2017 Ha-Duong Nguyen (@cmpitg)
+;;  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014-2018 Ha-Duong Nguyen (@cmpitg)
 ;;
 ;; This project is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
