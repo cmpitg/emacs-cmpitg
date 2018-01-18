0fd01ae8ba543d294fdc2b6e30349b0a1bfc4c8e
Ha-Duong Nguyen <cmpitg@gmail.com>
2018-01-18 19:38:06 +0200

    feat: Add partial testing scenario

---
+++ src/testing-scenario.el
@@ -0,0 +1,98 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Firefox
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; - Call `~firefox`
+;; - Call `~firefox` with a selection which is a URL
+;; - Call `~firefox` with a cursor under a URL
+
+;; https://yle.fi
+;; https://areena.yle.fi
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Asciidoc mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Create/open an Asciidoc file and try:
+;; - Previewing
+;; - Making change then previewing
+;; - Rendering to a specific location
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; *exec* functions & calling external processes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Execute a command and return the output as string.
+;; Quick pattern: <not-possible>
+(~exec "ls")
+
+;; Execute a command synchronously, taking current region as stdin and return
+;; the output as string.
+;; Quick pattern: <not-possible>
+(~exec "cat" :stdin :region)
+
+;; Execute a command synchronously, taking current region as stdin and return
+;; the output as string.
+;; Quick pattern: <not-possible>
+(~exec "cat" :stdin "Some\ncrap")
+
+;; Execute a command and pop up a temporary buffer showing result.
+;; Quick pattern: ls
+(~exec-pop-up "ls -lahF / /mnt")
+
+;; Execute a command, taking input from the current region and pop up a
+;; temporary buffer showing result.
+;; Test: select any text in any window, then `eval-expression` the following.
+;; Quick pattern: >cat
+(~exec> "cat")
+(~exec> "no-command")
+
+;; Execute a command, taking input from the current region replace it with
+;; result once the command is done.
+;; Test: select any text in any window, then `eval-expression` the following.
+;; Quick pattern: |sort
+(~exec| "cat")
+(~exec| "sort")
+(~exec| "no-command")
+
+;; b
+;; a
+;; d
+;; c
+
+;; Execute a command and replace current region with the result once the
+;; command is done.
+;; Test: Place the cursor 
+;; Quick pattern: <ls
+(~exec< "date")
+(~exec< "date -R")
+
+;; Run an external processes
+(~run-process (format "web-browser-gui %s"
+                      (shell-quote-argument "https://yle.fi")))
+
+(~run-process (format "web-browser-gui %s"
+                      (shell-quote-argument "https://yle.fi"))
+              :async nil)
+
+;; Open with an external program
+(~open-with "https://svenska.yle.fi/" "web-browser-gui %s")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Click & execute
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; b
+;; a
+;; e
+;; c
+;; d
+
+;; <ls
+;; <date
+;; <date -R
+;; >cat
+;; >sort
+;; >rot13
+;; |sort
+;; |rot13
