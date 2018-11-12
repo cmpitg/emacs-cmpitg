56fde1b7115e6612d38d20e0c273852fd2e9d9eb
Ha-Duong Nguyen <cmpitg@gmail.com>
2018-11-12 22:24:02 +0200

    feat: ~toggle-toolbox follows the directory of the prev buffer

--- src/config-core-functions.el
+++ src/config-core-functions.el
@@ -287,12 +287,18 @@ The buffered used for input uses `prompt' as it title and
       (set-window-dedicated-p (selected-window) t))
     current-buffer))
 
-(defun* ~toggle-toolbox (&key (path *toolbox-path*) (side 'left) (size 70))
+(defun* ~toggle-toolbox (&key (path *toolbox-path*)
+                              (side 'left)
+                              (size 70)
+                              (follow-dir t))
   "Toggles toolbox file.  The path to the toolbox file is passed
 on using the `path' argument.  The toolbox window is sticky,
-appears on the `side', and using `size' as its width."
+appears on the `side', and using `size' as its width.
+`follow-dir' determines whether or not the toolbox buffer
+inherits the working directory from the buffer that calls it."
   (interactive)
-  (let ((toolbox-buffer-name (file-name-nondirectory path)))
+  (let ((toolbox-buffer-name (file-name-nondirectory path))
+        (working-dir default-directory))
     (if-let (wind (get-buffer-window toolbox-buffer-name))
         (delete-window wind)
       (progn
@@ -300,6 +306,7 @@ appears on the `side', and using `size' as its width."
         (let ((split-window-preferred-function nil)
               (pop-up-windows nil))
           (~smart-open-file path))
+        (when follow-dir (setq-local default-directory working-dir))
         (set-window-dedicated-p (selected-window) t)))))
 
 (defun ~toggle-maximize-buffer ()
