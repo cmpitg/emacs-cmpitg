f9e31ff375851c4dd7d72d14b4df82238203009c
Ha-Duong Nguyen <cmpitg@gmail.com>
2019-02-06 12:04:05 +0200

    feat: Various improvements for the header line
    - Make the closing/killing commands explicit: x (for frame), x-buffer (for
      buffer), and x-window (for window)
    
    - Make the buffer list command explicit: Buffers.  Note that this command will
      be reimplemented at some point.
    
    - Add a command to visit Toolbox: Tool
    
    - Add commands to send the usual "accept" or "discard" keybindings (C-c C-c
      and C-c C-k correspondingly)
    
    - Move the path to the end
    
    - Remove some rarely used commands

--- src/config-header-line.el
+++ src/config-header-line.el
@@ -1,7 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
 ;;
-;; Copyright (C) 2018 Ha-Duong Nguyen (@cmpitg)
+;; Copyright (C) 2018-2019 Ha-Duong Nguyen (@cmpitg)
 ;;
 ;; This project is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
@@ -28,30 +28,30 @@
 
 (setq-default header-line-format
               `(""
+                "x"
+                ,*header-line-separator*
+                "x-buffer"
+                ,*header-line-separator*
+                "x-window"
+                ,*header-line-separator*
+                "Buffers"
+                ,*header-line-separator*
+                "Tool"
+                ,*header-line-separator*
+                "/"
+                ,*header-line-separator*
+                "✓"
+                ,*header-line-separator*
+                "✗"
+                ,*header-line-separator*
+                "⊞+"
+                ,*header-line-separator*
+                "⇌"
+                ,*header-line-separator*
                 (:eval (if (buffer-file-name)
                            (~shorten-string buffer-file-name
                                             *header-line-max-path-length*)
                          ""))
-                ,*header-line-separator*
-                "╳"
-                ,*header-line-separator*
-                "x"
-                ,*header-line-separator*
-                "B"
-                ,*header-line-separator*
-                "/"
-                ,*header-line-separator*
-                "⊞+"
-                ,*header-line-separator*
-                "⊞-"
-                ,*header-line-separator*
-                "➡+"
-                ,*header-line-separator*
-                "⬇+"
-                ,*header-line-separator*
-                "⇌"
-                ,*header-line-separator*
-                "git"
                 ,*header-line-separator*
                 "+"))
 
@@ -66,25 +66,37 @@
            (kill-new buffer-file-name)
            (message "File path %s copied to clipboard" buffer-file-name))
           ((string= "x" str)
+           (call-interactively 'delete-frame))
+          ((string= "x-buffer" str)
            (call-interactively 'kill-this-buffer))
+          ((string= "x-window" str)
+           (call-interactively '~delete-window))
           ((string= "╳" str)
            (call-interactively '~delete-window))
-          ((string= "B" str)
+          ((string= "Buffers" str)
            (call-interactively 'ibuffer))
           ((string= "/" str)
            (dir-browser:render-dir-buffer default-directory))
+          ((string= "✓" str)
+           (call-interactively (key-binding (kbd "C-c C-c"))))
+          ((string= "✗" str)
+           (call-interactively (key-binding (kbd "C-c C-k"))))
           ((string= "⊞+" str)
            (call-interactively 'make-frame))
+          ((string= "⊞-" str)
+           (call-interactively 'delete-frame))
           ((string= "➡+" str)
            (call-interactively 'split-window-horizontally))
           ((string= "⬇+" str)
            (call-interactively 'split-window-vertically))
           ((string= "⇌" str)
            (call-interactively '~transpose-windows))
-          ((string= "+" str)
-           (call-interactively '~header-line-add))
           ((string= "git" str)
            (call-interactively 'magit-status))      
+          ((string= "Tool" str)
+           (find-file *toolbox-path*))
+          ((string= "+" str)
+           (call-interactively '~header-line-add))
           ((string= *header-line-separator* str)
            nil)
           (t
