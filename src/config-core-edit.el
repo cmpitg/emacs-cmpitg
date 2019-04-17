96a8018047f17e643206fad1101d57eeef8aeac8
Ha-Duong Nguyen <cmpitg@gmail.com>
2019-04-17 16:46:22 +0300

    doc: Update year in the license header

--- src/config-core-edit.el
+++ src/config-core-edit.el
@@ -1,5 +1,5 @@
 ;;
-;; Copyright (C) 2018 Ha-Duong Nguyen (@cmpitg)
+;; Copyright (C) 2018-2019 Ha-Duong Nguyen (@cmpitg)
 ;;
 ;; This project is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
