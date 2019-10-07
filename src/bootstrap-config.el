572031c80dca9edeae960da1b135eca4e791c01d
Ha-Duong Nguyen <cmpitg@gmail.com>
2019-10-07 16:33:20 +0300

    ensure-frame only creates frame if there is none

--- src/bootstrap-config.el
+++ src/bootstrap-config.el
@@ -36,7 +36,10 @@
            (display-re (and display-list (regexp-opt display-list)))
            (term (and display-re (cl-some (lambda (term) (and (string-match display-re (terminal-name term)) term)) (terminal-list))))
            (frame (and term (cl-some (lambda (frame) (and (frame-live-p frame) frame)) (frames-on-display-list term)))))
-      (select-frame (or frame (make-frame-on-display (getenv "DISPLAY"))))))
+      (unless frame
+        (select-frame (make-frame-on-display (getenv "DISPLAY"))))
+      ;; (select-frame (or frame (make-frame-on-display (getenv "DISPLAY"))))
+      ))
   (advice-add 'y-or-n-p :before #'~advice/ensure-frame)
   (advice-add 'yes-or-no-p :before #'~advice/ensure-frame)
   (advice-add 'read-passwd :before #'~advice/ensure-frame))
