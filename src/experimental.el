d2677b836342b6b1c589abdf3d5fdab428002d0b
Ha-Duong Nguyen <cmpitg@gmail.com>
2020-02-04 18:34:40 +0200

    feat: Archive unused eshell config

--- src/experimental.el
+++ src/experimental.el
@@ -1,5 +1,5 @@
 ;;
-;; Copyright (C) 2014-2019 Ha-Duong Nguyen (@cmpitg)
+;; Copyright (C) 2014-2020 Ha-Duong Nguyen (@cmpitg)
 ;;
 ;; This project is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
@@ -22,13 +22,11 @@
 ;; Aliases
 ;;
 
-(defalias '+cp #'command-palette:ensure-command-palette)
 (defalias '+col #'split-window-horizontally)
 (defalias '+row #'split-window-vertically)
 (defalias '+buf #'~new-buffer)
 (defalias '+frame #'make-frame)
 (defalias '+-tool #'~toggle-project-toolbox)
-(defalias '+bm #'~append-pos-to-window-on-the-right)
 (defalias '-buf #'kill-this-buffer)
 (defalias '-window #'~delete-window)
 (defalias '-me #'~kill-buffer-and-window)
@@ -53,76 +51,6 @@
   (progn
     (auto-compile-on-load-mode)
     (auto-compile-on-save-mode)))
-
-;;
-;; Eshell utilization
-;;
-
-(defun ~eshell-dir (dir)
-  "Starts Eshell in a directory.  If Eshell has already been
-started, change the directory."
-  (interactive "DDirectory: ")
-  (cond
-   ((get-buffer "*eshell*")
-    (with-current-buffer "*eshell*"
-      (cd dir)
-      (eshell-emit-prompt))
-    (switch-to-buffer "*eshell*"))
-   (t
-    (cd dir)
-    (eshell))))
-
-(defun ~eshell-current-project-dir ()
-  "Starts Eshell in the current project directory or the current
-directory."
-  (interactive)
-  (~eshell-dir (~current-project-root)))
-
-(defun ~eshell-quit ()
-  "Quits Eshell when current command is empty."
-  (interactive)
-  (insert "exit")
-  (eshell-send-input))
-
-(defun ~my/eshell-prompt-function ()
-  (format " - %s %s@%s %s -\n%s "
-          (format-time-string "%H:%M:%S" (current-time))
-          (user-login-name)
-          (system-name)
-          (eshell/pwd)
-          (if (zerop (user-uid)) "#" "$")))
-
-;; (defun ~my/eshell-prompt-function ()
-;;   (format " - %s %s@%s %s -\n"
-;;           (format-time-string "%H:%M:%S" (current-time))
-;;           (user-login-name)
-;;           (system-name)
-;;           (eshell/pwd)))
-
-(defun ~my/eshell-maybe-bol ()
-  "Goes to beginning of command line first, then beginning of
-line in Eshell."
-  (interactive)
-  (let ((p (point)))
-    (eshell-bol)
-    (when (= p (point))
-      (beginning-of-line))))
-
-(require 'eshell)
-(require 'em-smart)
-(setq eshell-where-to-jump 'begin)
-(setq eshell-review-quick-commands nil)
-(setq eshell-smart-space-goes-to-end t)
-
-(bind-key "<M-insert>" '~eshell-current-project-dir)
-
-;; (setq eshell-prompt-function #'~my/eshell-prompt-function)
-;; (setq eshell-prompt-regexp (rx bol (or "#" "$") " "))
-;; (setq eshell-prompt-regexp "")
-(add-hook 'eshell-mode-hook
-          #'(lambda ()
-              (bind-key "s-d" #'~my/eshell-maybe-bol eshell-mode-map)
-              (evil-define-key 'normal eshell-mode-map (kbd "0") #'~my/eshell-maybe-bol)))
 
 ;; (setenv "XDG_DATA_DIRS" "/usr/share/i3:/usr/local/share:/usr/share")
 
