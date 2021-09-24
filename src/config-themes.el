4df6199088e0f312b7c4271e42d6f0cbbc4bdc74
Ha-Duong Nguyen <cmpitg@gmail.com>
2021-09-25 01:52:58 +0300

    feat: Upgrade modul-operandi theme

--- src/config-themes.el
+++ src/config-themes.el
@@ -42,10 +42,20 @@
 ;; (load-theme 'cmpitg-thursday t)
 ;; (load-theme 'cmpitg-random-light t)
 
-(use-package modus-operandi-theme
+(use-package modus-themes
+  :ensure
   :init
-  (progn
-    (load-theme 'modus-operandi t)))
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-italic-constructs t
+        modus-themes-bold-constructs nil
+        modus-themes-region '(bg-only no-extend))
+
+  ;; Load the theme files before enabling a theme
+  (modus-themes-load-themes)
+  :config
+  ;; Load the theme of your choice:
+  (modus-themes-load-operandi))
+;; OR (modus-themes-load-vivendi))
 
 ;; Current
 ;; (load-theme 'plan9 t)
