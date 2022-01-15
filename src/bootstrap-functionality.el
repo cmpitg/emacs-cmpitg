455ef275b088da619dc3d33bf52cf63517bc9e37
Ha-Duong Nguyen <cmpitg@gmail.com>
2022-01-15 23:27:09 +0200

    feat: Explicitly load color

--- src/bootstrap-functionality.el
+++ src/bootstrap-functionality.el
@@ -28,7 +28,8 @@
 
 (eval-when-compile
   (require 'cl)
-  (require 'cl-lib))
+  (require 'cl-lib)
+  (require 'color))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UX
