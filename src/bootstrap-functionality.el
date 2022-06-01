5b083d1ec75c64d9a78443aa56157070818cbd51
Ha-Duong Nguyen <cmpitg@gmail.com>
2022-06-02 00:32:25 +0300

    feat: Try requiring org-mode

--- src/bootstrap-functionality.el
+++ src/bootstrap-functionality.el
@@ -1839,6 +1839,7 @@ line in Eshell."
 (bind-key "M-SPC l l" #'find-library)
 
 ;; Org
+(require 'org)
 (with-eval-after-load "org"
   (let ((repeatable-keybindings `(("i" . org-insert-heading)
                                   ("I" . org-insert-item)
