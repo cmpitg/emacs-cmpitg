a095b89692d35400bcb45511a49e5f52d610f844
Ha-Duong Nguyen <cmpitg@gmail.com>
2025-11-13 15:27:50 +0200

    fix: ~get-current-project-root now expands the file name

--- src/bootstrap-functionality.el
+++ src/bootstrap-functionality.el
@@ -616,7 +616,7 @@ Source: http://stackoverflow.com/a/4717026/219881"
 
   (defun ~get-current-project-root ()
     "Returns the current project root or current directory."
-    (or (ignore-errors (car (last (project-current))))
+    (or (ignore-errors (expand-file-name (car (last (project-current)))))
         default-directory))
 
   (defun ~expand-path-fully (path)
